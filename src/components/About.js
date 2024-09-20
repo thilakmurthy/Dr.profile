d7d3e56eaa835b061ec0250cf4c2d424745b30cc
Thilak K G <thilak.gurumurthy@gmail.com>
2024-09-20 15:31:12 +0530

    name change1

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ const About = () => {
             <div className='container'>
                 <img src={raju} alt='Raju' />
                 <div className='col-2'>
-                    <h2 style={{color:"#1d3d74"}}>About Dr. Raju R</h2>
+                    <h2 style={{color:"#1d3d74"}}>About Dr. Thilak </h2>
                     <span className='line'></span>
                     <p>Dr Raju R is a well-known Urologist currently associated with Cura Hospital, Kanakapura, in Bangalore. He has 12 years of experience in Urology and worked as an expert Urologist in different cities in India.</p>
                     <p>Doctor Raju R has contributed to handling numerous complex medical cases in several hospitals. He is known for his attention to accurate diagnosis and for treating patients empathetically. The speciality interests of Dr Raju R are Orchidectomy, Balanoposthitis, Circumcision, Cystolitholapaxy, Hemodialysis, Peritoneal Dialysis etc. </p>
